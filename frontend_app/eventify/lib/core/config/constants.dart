49df35c53a9ce08a1ea3e170083efc704789ad1c
G1-raj <gullurajput18@gmail.com>
2026-01-11 23:11:03 +0530

    added get events route

--- frontend_app/eventify/lib/core/config/constants.dart
+++ frontend_app/eventify/lib/core/config/constants.dart
@@ -1,5 +1,15 @@
 final String _baseUrl = "http://localhost:8000/";
 
+
+//auth routes
+
+//post route required email username full name and password in request body
 final String signupUrl = "$_baseUrl/auth/signup";
 
+//post route required email and password in request body
 final String loginUrl = "$_baseUrl/auth/login";
+
+//public route
+
+//get route to get all the events for users home screen
+final String getEvents = "$_baseUrl/public/all";
